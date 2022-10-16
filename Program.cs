aff1f1f424f341ce99a89d7f507064563c060836
Dmitry <nekipelovdima4@gail.com>
2022-10-17 01:23:09 +0300

    Создали новый метод

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿string[] arr1 = new string[5] {"678", "22", "hello", "world", "pan"};
 string[] arr2 = new string[arr1.Length];
+void SecondArray(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++) 
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
