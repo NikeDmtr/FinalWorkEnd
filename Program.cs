93b1490707b710c24ef6dd2fcd11abac7129a67f
NikeDmtr <112286065+NikeDmtr@users.noreply.github.com>
2022-10-21 21:17:56 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ void PrintArray(string[] arr)
 {
     for (int i = 0; i < arr.Length; i++)
     {
-        Console.Write("{arr$[i]} ");
+        Console.Write($"{arr[i]} ");
     }
     Console.WriteLine();
 }
